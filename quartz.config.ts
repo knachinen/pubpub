500dbe6de489305f7f1dcab2180a81fa851923de
faf <knachinen@gmail.com>
2024-04-24 01:16:09 +0900

    Quartz sync: Apr 24, 2024, 1:16 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🖨️ pubpub...",
+    pageTitle: "📠 pubpub...",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
